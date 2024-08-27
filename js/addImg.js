dd3ce1581a94a749216e3ba109c355e4f9b41849
bluecoder01 <onesixteenj@gmail.com>
2024-08-27 11:42:59 +0100

    Job Seekers 99%

--- js/addImg.js
+++ js/addImg.js
@@ -28,15 +28,12 @@ function getFile2() {
     if (input.files && input.files[0]) {
       const file = input.files[0];
 
-      if (!file.type.startsWith("image/")) {
-        alert("Please select an image file.");
-        return;
-      }
+    
 
       const reader = new FileReader();
 
       reader.onload = function (e) {
-        document.getElementById("addImgBtn-2").src = e.target.result;
+        document.getElementById("filename").innerText = "Resume.pdf";
       };
 
       reader.readAsDataURL(file);
--- js/nav.js
+++ js/nav.js
@@ -8,3 +8,24 @@ addEventListener("load", () => {
     })
 
 });
+
+const userMenu = document.querySelector('.user-menu');
+const imgButton = document.querySelector('.menu-trigger');
+
+// Event listener for button clicks
+  imgButton.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent the click event from propagating to the window
+    userMenu.classList.toggle('active');
+  });
+
+// Close the container when clicking outside of it
+window.addEventListener('click', (event) => {
+  if (!userMenu.contains(event.target)) {
+    userMenu.classList.remove('active');
+  }
+});
+
+// Prevent closing when clicking inside the result container
+userMenu.addEventListener('click', (event) => {
+  event.stopPropagation();
+});

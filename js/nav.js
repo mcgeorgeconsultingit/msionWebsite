7e1326b8a80d53fb724b2f05533b9993ebacdaca
bluecoder01 <onesixteenj@gmail.com>
2024-08-27 20:48:23 +0100

    final job seeker commit

--- js/nav.js
+++ js/nav.js
@@ -13,7 +13,7 @@ const userMenu = document.querySelector('.user-menu');
 const imgButton = document.querySelector('.menu-trigger');
 
 // Event listener for button clicks
-  imgButton.addEventListener('click', (event) => {
+  imgButton?.addEventListener('click', (event) => {
     event.stopPropagation(); // Prevent the click event from propagating to the window
     userMenu.classList.toggle('active');
   });
@@ -26,6 +26,6 @@ window.addEventListener('click', (event) => {
 });
 
 // Prevent closing when clicking inside the result container
-userMenu.addEventListener('click', (event) => {
+userMenu?.addEventListener('click', (event) => {
   event.stopPropagation();
 });
